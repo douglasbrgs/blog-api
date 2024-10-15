2b6036593f85f5e107d16864c0f8942b73f30fa6
Douglas Borges <douglasborges19@gmail.com>
2024-10-15 09:47:54 -0300

    JWT Claims

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,7 +6,7 @@ namespace Blog.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        [HttpPost("v1/login")]
+        [HttpGet("v1/login")]
         public IActionResult Login([FromServices] TokenService tokenService)
         {
             var token = tokenService.GenerateToken(null);
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Blog.Services
@@ -14,6 +15,12 @@ namespace Blog.Services
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new(ClaimTypes.Name, "douglasborges"), // User.Identity.Name
+                    new(ClaimTypes.Role, "admin") // User.InRole
+                }),
+
                 Expires = DateTime.UtcNow.AddHours(8),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
