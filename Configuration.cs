9727ad51c598902d0b04a07e9aef9ed8476bab3c
Douglas Borges <douglasborges19@gmail.com>
2024-10-15 12:21:48 -0300

    Implementando um ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -3,4 +3,8 @@
 public static class Configuration
 {
     public static string JwtKey { get; set; } = "YjViYmNlM2MtM2M2OC00ZjZlLTg0Y2MtNGUyOTUyNjM3ZDEw";
+
+    public static string ApiKeyName = "api_key";
+
+    public static string ApiKey = "curso_api_8Rjj0C87rU2s/i9bHdQZog==";
 }
