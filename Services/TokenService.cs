d420f6fd56a85038ce6eae548204abb374441b27
Douglas Borges <douglasborges19@gmail.com>
2024-10-15 08:51:24 -0300

    Gerando token

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -12,7 +12,11 @@ namespace Blog.Services
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
 
-            var tokenDescriptor = new SecurityTokenDescriptor();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
