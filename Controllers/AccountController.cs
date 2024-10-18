3017e6b06fd64cf2b4ee4af74166f6ad0021e8df
Douglas Borges <douglasborges19@gmail.com>
2024-10-17 21:34:41 -0300

    Upload de imagem

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,10 +2,13 @@
 using Blog.Extensions;
 using Blog.Models;
 using Blog.Services;
-using Blog.ViewModels;
+using Blog.ViewModels.Accounts;
+using Blog.ViewModels.Categories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
+using System.Text.RegularExpressions;
 
 namespace Blog.Controllers
 {
@@ -96,6 +99,44 @@ namespace Blog.Controllers
             {
                 return StatusCode(500, new ResultViewModel<string>("Falha interna do servidor"));
             }
+        }
+
+        [Authorize]
+        [HttpPost("v1/accounts/upload-image")]
+        public async Task<IActionResult> UploadImage(
+            [FromBody] UploadImageViewModel model,
+            [FromServices] BlogDataContext context)
+        {
+            var fileName = $"{Guid.NewGuid().ToString()}.jpg";
+            var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(model.Base64Image, "");
+            var bytes = Convert.FromBase64String(data);
+
+            try
+            {
+                await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna no servidor"));
+            }
+
+            var user = await context.Users.FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+            if (user == null)
+            {
+                return NotFound(new ResultViewModel<string>("Usuário não encontrado"));
+            }
+
+            user.Image = $"https://localhost:0000/images/{fileName}";
+            try
+            {
+                context.Users.Update(user);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna do servidor"));
+            }
+
+            return Ok(new ResultViewModel<string>("Imagem alterada com sucesso.", null));
         }
     }
 }
