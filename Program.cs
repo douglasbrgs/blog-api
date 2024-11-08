a5298ad19eacf145ad30505b58b968390fc98022
Douglas Borges <douglasborges19@gmail.com>
2024-11-08 15:37:48 -0300

    Criados métodos de extensão para o builder

--- Program.cs
+++ Program.cs
@@ -1,25 +1,15 @@
-using Blog;
-using Blog.Data;
-using Blog.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.IO.Compression;
-using System.Text;
-using System.Text.Json.Serialization;
+using Blog.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-ConfigureAuthentication(builder);
-ConfigureMvc(builder);
-ConfigureServices(builder);
+builder.LoadConfiguration();
+builder.ConfigureAuthentication();
+builder.ConfigureMvc();
+builder.ConfigureServices();
 
-// Adiciona swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-LoadConfiguration(app);
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
@@ -35,71 +25,3 @@ if (app.Environment.IsDevelopment())
 }
 
 app.Run();
-
-void LoadConfiguration(WebApplication app)
-{
-    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
-    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
-    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
-
-    var smtp = new Configuration.SmtpConfiguration();
-    app.Configuration.GetSection("Smtp").Bind(smtp);
-    Configuration.Smtp = smtp;
-}
-
-void ConfigureAuthentication(WebApplicationBuilder builder)
-{
-    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-
-    builder.Services
-        .AddAuthentication(x =>
-        {
-            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(x => x.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        });
-}
-
-void ConfigureMvc(WebApplicationBuilder builder)
-{
-    builder.Services.AddMemoryCache();
-
-    builder.Services.AddResponseCompression(options =>
-    {
-        options.Providers.Add<GzipCompressionProvider>();
-        options.EnableForHttps = true;
-    });
-
-    builder.Services.Configure<GzipCompressionProviderOptions>(options =>
-    {
-        options.Level = CompressionLevel.Optimal;
-    });
-
-    builder.Services
-    .AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-
-    })
-    .AddJsonOptions(x =>
-    {
-        x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
-    });
-}
-
-void ConfigureServices(WebApplicationBuilder builder)
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<BlogDataContext>(options => options.UseSqlServer(connectionString));
-
-    builder.Services.AddTransient<TokenService>();
-    builder.Services.AddTransient<EmailService>();
-}
